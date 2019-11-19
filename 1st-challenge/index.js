00c1a6bf92b97bfdb70e000c3128d093b1d3273e
Tloz-z <sksmsdldjeh123@gmail.com>
2019-11-19 11:47:32 +0900

    reactJS (#1.6 ~ #1.8)

--- 1st-challenge/index.js
+++ 1st-challenge/index.js
@@ -1,14 +1,23 @@
+// class ObjectUtilities {
+//     static mergeObjects = (objA, objB) => Object.assign(objA, objB);
+//     static removePassword(user) {
+//       const extra = { ...user };
+//       delete extra.password;
+//       return extra;
+//     }
+//     static freezeObj = cleanUser => Object.freeze(cleanUser);
+//     static getOnlyValues = frozenUser => Object.values(frozenUser);
+//     static getOnlyProperties = frozenUser => Object.keys(frozenUser);
+//   }
+
+// 위에가 제출한 것 밑에가 답안
 class ObjectUtilities {
-    static mergeObjects = (objA, objB) => Object.assign(objA, objB);
-    static removePassword(user) {
-      const extra = { ...user };
-      delete extra.password;
-      return extra;
-    }
-    static freezeObj = cleanUser => Object.freeze(cleanUser);
-    static getOnlyValues = frozenUser => Object.values(frozenUser);
-    static getOnlyProperties = frozenUser => Object.keys(frozenUser);
-  }
+  static mergeObjects = (objA, objB) => ({...objA , ...objB}); // 오브젝트를 표현할때 () 필수인듯
+  static removePassword = ({password , ...rest}) => rest; //여기도 마찬가지
+  static freezeObj = cleanUser => Object.freeze(cleanUser);
+  static getOnlyValues = frozenUser => Object.values(frozenUser);
+  static getOnlyProperties = frozenUser => Object.keys(frozenUser);
+}
   
   const objA = {
     name: "Nicolas",
--- practice/src/index.js
+++ practice/src/index.js
@@ -25,4 +25,20 @@ const allDays = [...days , ...otherDays , "sun"];
 
 console.log(allDays);
 
-//------------------------------------------
+// # 1.6-------------------------------------
+
+const numberDays = allDays.map((day , index) => `#${index + 1} ${day}`);
+
+console.log(numberDays);
+
+// # 1.7-------------------------------------
+
+const numbers = [3 , 14, 123, 132, 12, 33, 1 , 3, 4, 5];
+
+const cleanNumbers = numbers.filter(number => number < 15);
+
+console.log(cleanNumbers);
+
+// # 1.8 여러가지 배열 메소드
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array
+// 위 사이트를 참고
